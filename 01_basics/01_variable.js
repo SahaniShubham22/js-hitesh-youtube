7fdc6876533a445b2032a35278c7544004f47f28
SahaniShubham22 <154399760+SahaniShubham22@users.noreply.github.com>
2023-12-20 09:04:10 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountId = 122334
+let emailId = "sahani@gmail.com"
+var accountpassword = "sahani@123"
+accountpassword = "sahaniyheiue83"
+emailId = "jdhhdjhoik78478"
+let city
+console.log([emailId ,accountId,accountpassword,city]);
